e08a75c63d21beba3044cdca3abdce6453f54cee
Yeremia395 <53548038+Yeremia395@users.noreply.github.com>
2019-08-22 13:49:22 +0700

    first commit

---
+++ src/pratikum/pkg2/Latihan3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratikum.pkg2;     
+
+/**
+ *
+ * @author Yeremia Rizky
+ */
+public class Latihan3 {
+     /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+    int x = 10;
+    int y = 3;
+    double z = 2.5;
+    
+    int a = 0;
+    double b = 0;
+    
+    //Tambahkan sintaks operasi matematika di baris ini
+    
+    a = x / y ;
+    b = x / z ;
+    
+    System.out.println(a);
+    System.out.println(b);
+    }
+}
---
+++ src/pratikum/pkg2/Pratikum2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratikum.pkg2;
+
+/**
+ *
+ * @author Yeremia Rizky
+ */
+public class Pratikum2 {
+
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 2;
+        
+        
+        
+        
+        int c;
+        
+        // pemjumlahan
+        c = a + b;
+        System.out.println(a + " + " + b + " = " + c );
+        
+        // pengurangan
+        c = a - b;
+        System.out.println(a + " - " + b + " = " + c );
+        
+        // perkalian
+        c = a * b;
+        System.out.println(a + " x " + b + " = " + c );
+        
+        // pembagian
+        c = a / b;
+        System.out.println(a + " : " + b + " = " + c );
+        
+    }
+    
+}
